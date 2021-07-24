f291771f9871de1d16f947d28dd694f0076a0fd1
Sam Kozin <sam.kozin@gmail.com>
2021-07-24 23:34:18 +0200

    add HMAC-SHA256 signature

--- client/crypto.py
+++ client/crypto.py
@@ -1,3 +1,6 @@
+import hmac
+import hashlib
+
 from Crypto import Random
 from Crypto.Cipher import AES
 
@@ -6,18 +9,28 @@ def generate_key():
   return Random.new().read(32)
 
 
-def aes_cbc_encrypt(raw, key):
+def encrypt(raw, key):
   iv = Random.new().read(AES.block_size)
   cipher = AES.new(key, AES.MODE_CBC, iv)
+  data = cipher.encrypt(pkcs7_pad(raw, AES.block_size))
+  hmac = hmac_sha256_sign(key, data + iv)
   return {
+    'data': data,
+    'hmac': hmac,
     'iv': iv,
-    'result': cipher.encrypt(pkcs7_pad(raw, AES.block_size))
   }
 
 
-def aes_cbc_decrypt(enc, iv, key):
+def decrypt(data, iv, hmac, key):
+  expected_hmac = hmac_sha256_sign(key, data + iv)
+  if hmac != expected_hmac:
+      raise ValueError('HMAC verification failed')
   cipher = AES.new(key, AES.MODE_CBC, iv)
-  return pkcs7_unpad(cipher.decrypt(enc), AES.block_size)
+  return pkcs7_unpad(cipher.decrypt(data), AES.block_size)
+
+
+def hmac_sha256_sign(message, key):
+  return hmac.new(key, msg=message, digestmod=hashlib.sha256).digest()
 
 
 def pkcs7_pad(data, block_size_bytes):
